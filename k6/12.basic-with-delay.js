5d61cefd0a0628a3e33428994f52824c77ff4bbc
Pepe Cano <825430+ppcano@users.noreply.github.com>
2023-06-16 20:22:01 +0200

    Update `12.basic-with-delay` test to allow inject fault to multiple targets

--- k6/12.basic-with-delay.js
+++ k6/12.basic-with-delay.js
@@ -4,31 +4,35 @@ import { ServiceDisruptor } from "k6/x/disruptor";
 
 const BASE_URL = __ENV.BASE_URL || "http://localhost:3333";
 
-export const options = {
-  scenarios: {
-    load: {
-      executor: "constant-vus",
-      vus: 5,
-      duration: "30s",
-      startTime: "10s",
-    },
-    disrupt: {
-      executor: "shared-iterations",
-      iterations: 1,
-      vus: 1,
-      exec: "disrupt",
-    },
+const scenarios = {
+  disrupt: {
+    executor: "shared-iterations",
+    iterations: 1,
+    exec: "disrupt",
   },
+  load: {
+    executor: "constant-vus",
+    vus: 5,
+    duration: "30s",
+    startTime: "10s",
+  },
+};
+if (__ENV.DISABLE_DISRUPT) {
+  delete scenarios["disrupt"];
+}
+
+export const options = {
+  scenarios: scenarios,
 };
 
 export function disrupt(data) {
   const disruptor = new ServiceDisruptor("pizza-info", "pizza-ns");
   const targets = disruptor.targets();
-  if (targets.length != 1) {
+  if (targets.length == 0) {
     throw new Error("expected list to have one target");
   }
 
-  disruptor.injectHTTPFaults({ averageDelay: "500ms" }, "40s");
+  disruptor.injectHTTPFaults({ averageDelay: "1000ms" }, "40s");
 }
 
 export default function () {
