d31aae506512c2036070722cfaa86ff5503cce6f
Roberto Santalla <roberto@santalla.io>
2023-08-30 18:11:06 +0200

    frontend/faro: read faro_url from `/api/config` Faro URL can be provided by starting the backend with the env var QUICKPIZZA_CONF_FARO_URL set to the desired Faro URL.

--- pkg/web/src/hooks.client.ts
+++ pkg/web/src/hooks.client.ts
@@ -1,20 +1,40 @@
-import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
-import { TracingInstrumentation } from '@grafana/faro-web-tracing';
-import { PUBLIC_FARO_WEBSDK_URL } from '$env/static/public'
+import {getWebInstrumentations, initializeFaro} from '@grafana/faro-web-sdk';
+import {TracingInstrumentation} from '@grafana/faro-web-tracing';
+import {PUBLIC_BACKEND_ENDPOINT} from '$env/static/public'
 
-initializeFaro({
-  url: PUBLIC_FARO_WEBSDK_URL,
-  app: {
-    name: 'QuickPizza',
-    version: '1.0.0',
-    environment: 'production'
-  },
-  instrumentations: [
-    // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
-    ...getWebInstrumentations(),
+function setupFaro() {
+    fetch(`${PUBLIC_BACKEND_ENDPOINT}api/config`).
+    then(data => data.json()).
+    then(config => {
+        const url = config.faro_url;
+        if (url === undefined) {
+            console.warn("Grafana faro is not configured.")
+            return
+        }
 
-    // Initialization of the tracing package.
-    // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
-    new TracingInstrumentation(),
-  ],
-});
+        console.log(`Initializing Grafana Faro to ${url}`)
+        initializeFaro({
+            url,
+            app: {
+                name: 'QuickPizza',
+                version: '1.0.0',
+                environment: 'production'
+            },
+            instrumentations: [
+                // Mandatory, overwriting the instrumentations array would cause the default instrumentations to be omitted
+                ...getWebInstrumentations(),
+
+                // Initialization of the tracing package.
+                // This packages is optional because it increases the bundle size noticeably. Only add it if you want tracing data.
+                new TracingInstrumentation(),
+            ],
+        });
+
+    }).
+    catch(e => {
+        console.error("Cannot read config from backend")
+        console.error(e.message)
+    });
+}
+
+setupFaro();
