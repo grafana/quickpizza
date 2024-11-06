c000ecf6f2e2a49d1e185bc2ae1cb25171cdf72b
Mihail Stoykov <312246+mstoykov@users.noreply.github.com>
2024-11-06 11:20:17 +0200

    Replace k6/experimental/tracing

--- k6/foundations/14.basic.tracing.js
+++ k6/foundations/14.basic.tracing.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
-import tracing from 'k6/experimental/tracing';
+import tracing from 'https://jslib.k6.io/http-instrumentation-tempo/1.0.0/index.js';
 
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:3333';
 
