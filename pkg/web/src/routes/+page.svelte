7144fc208d51a88c8b4b9f41ca84132827e5bbdb
Federico Tedin <federicotedin@gmail.com>
2024-11-21 12:38:58 +0100

    Fix inconsistent form field styles; illegible text

--- pkg/web/src/routes/+page.svelte
+++ pkg/web/src/routes/+page.svelte
@@ -146,7 +146,7 @@
 								bind:value={restrictions.maxCaloriesPerSlice}
 								type="number"
 								id="floating_filled"
-								class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
+								class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
 								placeholder=" "
 							/>
 							<label
@@ -165,7 +165,7 @@
 							/>
 							<label
 								for="floating_filled"
-								class="absolute text-sm text-gray-900 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-red-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+								class="absolute text-sm text-gray-900 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-red-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
 								>Min Number of Toppings</label
 							>
 						</div>
@@ -174,7 +174,7 @@
 								bind:value={restrictions.maxNumberOfToppings}
 								type="number"
 								id="floating_filled"
-								class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-red-600 peer"
+								class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
 								placeholder=" "
 							/>
 							<label
