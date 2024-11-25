31bdd7e46a86369e37738302ed8ac1652c2c53a8
Federico Tedin <federicotedin@gmail.com>
2024-11-25 15:00:21 +0100

    Add k6 test

---
+++ k6/foundations/16.grpc.js
@@ -0,0 +1,25 @@
+import { Client, StatusOK } from 'k6/net/grpc';
+import { check, sleep } from 'k6';
+
+const BASE_URL = 'localhost:3334';
+
+const client = new Client();
+client.load(['definitions'], '../../../proto/quickpizza.proto');
+
+export default () => {
+  client.connect(BASE_URL, {
+    plaintext: true
+  });
+
+  const data = { ingredients: ["Pepperoni", "Mozzarella"], dough: "Stuffed" };
+  const response = client.invoke('quickpizza.GRPC/EvaluatePizza', data);
+
+  check(response, {
+    'status is OK': (r) => r && r.status === StatusOK,
+  });
+
+  console.log(JSON.stringify(response.message));
+
+  client.close();
+  sleep(1);
+};
