6845f261d9307e7db43b9ced2e7ef86b4cb87ca6
Federico Tedin <federicotedin@gmail.com>
2024-11-29 10:40:15 +0100

    Add user to admin login page

--- pkg/web/src/routes/admin/+page.svelte
+++ pkg/web/src/routes/admin/+page.svelte
@@ -5,6 +5,7 @@ import { userID } from '../../lib/stores';
 
 var user = 0;
 var loginError = '';
+var username = 'admin';
 var password = 'admin';
 var adminLoggedIn = false;
 var latestPizzaRecommendations: string[] = [];
@@ -28,7 +29,7 @@ function checkAdminLoggedIn() {
 }
 
 async function handleSubmit() {
-    const res = await fetch(`${PUBLIC_BACKEND_ENDPOINT}api/admin/login?user=admin&password=${password}`, {
+    const res = await fetch(`${PUBLIC_BACKEND_ENDPOINT}api/admin/login?user=${username}&password=${password}`, {
 			method: 'GET',
             headers: {
 					'X-User-ID': user.toString()
@@ -106,8 +107,12 @@ function updateRecommendations() {
                 </h1>
                 <form class="space-y-4 md:space-y-6" on:submit|preventDefault={handleSubmit}>
                     <div>
-                        <label for="text" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                        <input type="text" name="password" id="password" placeholder="admin" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required bind:value={password}>
+                        <label for="text" class="block mb-2 text-sm font-medium text-gray-900">Username (hint: admin)</label>
+                        <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required bind:value={username}>
+                    </div>
+                    <div>
+                        <label for="text" class="block mb-2 text-sm font-medium text-gray-900">Password (hint: admin)</label>
+                        <input type="text" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required bind:value={password}>
                     </div>
                     <button type="submit" class="w-full text-gray-900 bg-gray-50 hover:bg-gray-100 border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
                 </form>
