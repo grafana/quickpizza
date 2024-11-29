3f77cc39ef804a5045f83c975c2f147d63934a2a
Federico Tedin <federicotedin@gmail.com>
2024-11-29 10:30:46 +0100

    Fix admin page label colors

--- pkg/web/src/routes/admin/+page.svelte
+++ pkg/web/src/routes/admin/+page.svelte
@@ -101,12 +101,12 @@ function updateRecommendations() {
 	<div class="text-center">
         <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                     QuickPizza Administration
                 </h1>
                 <form class="space-y-4 md:space-y-6" on:submit|preventDefault={handleSubmit}>
                     <div>
-                        <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <label for="text" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
                         <input type="text" name="password" id="password" placeholder="admin" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required bind:value={password}>
                     </div>
                     <button type="submit" class="w-full text-gray-900 bg-gray-50 hover:bg-gray-100 border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
