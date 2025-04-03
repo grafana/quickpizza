258488bb8460d2939270b74586648bf2f098e0c4
Federico Tedin <federicotedin@gmail.com>
2025-04-03 14:58:30 +0200

    Change input types

--- pkg/web/src/routes/login/+page.svelte
+++ pkg/web/src/routes/login/+page.svelte
@@ -154,7 +154,7 @@
 								>Password (hint: 12345678)</label
 							>
 							<input
-								type="text"
+								type="password"
 								name="password"
 								id="password"
 								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
